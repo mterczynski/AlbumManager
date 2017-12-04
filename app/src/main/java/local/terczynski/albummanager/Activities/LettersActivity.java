824f30f6927e4b51c4c5229df0639a00c9520429
mterczynski <mterczynski271@gmail.com>
2017-12-04 14:07:55 +0100

    Added lettersActivity

---
+++ app/src/main/java/local/terczynski/albummanager/Activities/LettersActivity.java
@@ -0,0 +1,13 @@
+package local.terczynski.albummanager.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LettersActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_letters);
+    }
+}
