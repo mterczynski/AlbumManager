02bf60aebe3ea7eec88d54884ef2a764ea99467c
mterczynski <mterczynski271@gmail.com>
2017-12-18 13:41:51 +0100

    Added fonts activity layout

---
+++ app/src/main/java/local/terczynski/albummanager/Activities/FontsActivity.java
@@ -0,0 +1,15 @@
+package local.terczynski.albummanager.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import local.terczynski.albummanager.R;
+
+public class FontsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fonts);
+    }
+}
