177773ea16fef97366efe903812722538be0fcac
mterczynski <mterczynski271@gmail.com>
2017-12-18 17:52:42 +0100

    Added loading fonts

--- app/src/main/java/local/terczynski/albummanager/Activities/FontsActivity.java
+++ app/src/main/java/local/terczynski/albummanager/Activities/FontsActivity.java
@@ -2,6 +2,8 @@ package local.terczynski.albummanager.Activities;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.res.AssetManager;
+import android.graphics.Typeface;
 import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -9,7 +11,10 @@ import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import local.terczynski.albummanager.R;
 
@@ -21,6 +26,9 @@ public class FontsActivity extends AppCompatActivity {
     private EditText editText;
     private TextView outputText;
     private ImageView acceptButton;
+    private AssetManager assetManager;
+    private LinearLayout textViewList;
+    private String[] listOfFonts;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +39,26 @@ public class FontsActivity extends AppCompatActivity {
         acceptButton = findViewById(R.id.fonts_accept);
         outputText = findViewById(R.id.font_textOutput);
         editText = (EditText )findViewById(R.id.fonts_editText);
+        textViewList = (LinearLayout) findViewById(R.id.font_scrollView_linear);
+
+        // get assetManager:
+        assetManager = getAssets();
+        try{
+            listOfFonts = assetManager.list("fonts");
+            Toast.makeText(FontsActivity.this, "fonts loaded", Toast.LENGTH_SHORT).show();
+
+            // show fonts:
+            for(int i=0; i<listOfFonts.length; i++){
+                TextView chooseFont = new TextView(FontsActivity.this);
+                Typeface typeface = Typeface.createFromAsset(assetManager, "fonts/" + listOfFonts[i]);
+                chooseFont.setTypeface(typeface);
+                chooseFont.setText("Zażółć gęślą jaźń");
+                chooseFont.setTextSize(32f);
+                textViewList.addView(chooseFont);
+            }
+        } catch (Exception ex){
+            Toast.makeText(FontsActivity.this, "no fonts found", Toast.LENGTH_SHORT).show();
+        }
 
         Log.d("fonts", "init activity");
 
